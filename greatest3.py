85421f819cd64dbaf146687f50b11f1078e20639
AK-junior <codeninjak27@gmail.com>
2026-08-21 20:47:00 +0530

    Modyfying the code

--- greatest3.py
+++ greatest3.py
@@ -2,4 +2,12 @@
 a = int(input("Enter The first Number: "))
 b = int(input("Enter The Second Number: "))
 c =int(input("Enter The third Number: "))
-print(a,"is greater") if ((a > b) and (a > c)) else print(b,"is greater") if ((b > a) and (b > c)) else print(c,"is greater")
+# print(a,"is greater") if ((a > b) and (a > c)) else print(b,"is greater") if ((b > a) and (b > c)) else print(c,"is greater")
+if (a > b):
+	if (a > c):
+		print(a,"is greater")
+elif(b>a):
+	if(b > c):
+		print(b,"is greater")
+else:
+	print(c,"is greater")
