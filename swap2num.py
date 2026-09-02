41e515daaba7eca2298de1f54404c11992f97a52
AK-junior <codeninjak27@gmail.com>
2026-09-02 22:36:42 +0530

    adding swap2num.py

---
+++ swap2num.py
@@ -0,0 +1,11 @@
+# python script to swap 2 uder defined integers 
+a = int(input("Enter A :"))
+b = int(input("Enter B :"))
+print("Before :",a,",",b)
+# a = a+b
+# b = a-b
+# a = a-b
+
+# Python Flex :
+a,b = b,a 
+print("After : ",a,",",b)
