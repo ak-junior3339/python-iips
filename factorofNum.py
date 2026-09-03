c552024a4e22a5915f9df4bd1fd74aff1017b083
AK-junior <codeninjak27@gmail.com>
2026-09-03 22:25:38 +0530

    factors if number Prime classification

---
+++ factorofNum.py
@@ -0,0 +1,11 @@
+# write a python script to identify factors of a given integer then classify the factor in composite or prime
+def isp(num):
+	for i in range(2,n):
+		if(n%i)==0:
+			print("Composite")
+			break
+	else:
+		print("Prime")
+
+n = int(input("Enter the number : "))
+for i in range()
