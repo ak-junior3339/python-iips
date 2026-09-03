75f5bfd89739cfd792d906a9cadd0480b134f897
AK-junior <codeninjak27@gmail.com>
2026-09-03 22:14:19 +0530

    adding calulator.py

---
+++ Calculator.py
@@ -0,0 +1,5 @@
+# write a pythin script to make a cli calculator with following functionalities: 
+# it should perform aithemetic operaions
+# it must not stop working until user allow it
+# make a smart convertion if value generated has zero after decimal point it must be type casted to integer
+
