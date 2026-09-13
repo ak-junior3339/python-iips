bcb86e6b7e9f8bf09120ec066e53f369d2507943
AK-junior <codeninjak27@gmail.com>
2026-09-13 21:04:21 +0530

    adding remove duplicate files

---
+++ RemoveDuplicates.py
@@ -0,0 +1,9 @@
+def contains_duplicate(nums):
+    final_arr = []
+    for i in nums:
+        if i not in final_arr:
+            final_arr.append(i)
+    return len(final_arr) != len(nums)
+
+nums = list(map(int, input().split()))
+print(contains_duplicate(nums))
