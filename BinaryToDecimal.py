47b45c83bd396e8439c35b40e2fc0449146d095a
AK-junior <codeninjak27@gmail.com>
2026-09-13 22:58:08 +0530

    adding BinaryToDecimal

--- BinaryToDecimal.py
+++ BinaryToDecimal.py
@@ -1,1 +1,10 @@
-dict = {'0':1,'1':2,'2':4,'3':8,'4':16,'5':32,'6':64,'7':128,'8':256}
+# dict = {'0':1,'1':2,'2':4,'3':8,'4':16,'5':32,'6':64,'7':128,'8':256}
+arr = [1,2,4,8,16,32,64,128,256]
+num = input("enter the binary code : ")
+num = num[::-1]
+dec = 0
+for i in range(len(num)):
+	print(int(num[i]))
+	dec = dec + int(num[i]) * arr[i]
+
+print(dec)
