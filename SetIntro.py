c1a91c3271770c59f0246d10fb794b99b03405cf
AK-junior <codeninjak27@gmail.com>
2026-09-17 23:18:52 +0530

    adding python script

---
+++ SetIntro.py
@@ -0,0 +1,12 @@
+set_1 = {1,3,5,"12"}
+print(set_1)
+for i in set_1:
+	print(i)
+
+x = {} # this will be identified as a dictionary 
+print(type(x))
+y = set()
+print(type(y))
+set_1.add(12)
+print(set_1)
+
